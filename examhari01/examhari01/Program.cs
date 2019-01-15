f21ddfb9545451e23655a8996bc85b047f84baef
karantopandas <cristianotopandas007@gmail.com>
2019-01-15 17:36:28 +0700

    tugas hari 01

---
+++ examhari01/examhari01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examhari01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n=9;
+            //int w = 1;
+            //for (int i = 1; i < n; i++)
+            //{
+            //    Console.WriteLine("\t{0}{1}",i,w);
+                
+            //}
+            //Console.ReadKey();
+
+            soal03 soal02 = new soal03();
+        }
+    }
+
+   
+}
